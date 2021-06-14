3227e51bf78e927473e708cc89ecb551c555b5d3
Ahmad Zidni Zainul Ikhsan <azzidti34@gmail.com>
2021-06-14 15:36:44 +0700

    trash

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://native-mvc-php.com:85/public');
+
+//DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mvc_native_php');
