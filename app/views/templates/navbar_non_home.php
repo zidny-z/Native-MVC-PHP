bed98ba096b680c3b88f590001c6d43328c9cb71
Ahmad Zidni Zainul Ikhsan <azzidti34@gmail.com>
2021-06-18 09:46:18 +0700

    Fix All include content, asset n code
    image kegiatan content, navbar akehlah pkok

--- app/views/templates/navbar_non_home.php
+++ app/views/templates/navbar_non_home.php
@@ -6,16 +6,16 @@
 	          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 	          	<i class="el-icon-lines"></i>
 	          </button>
-	          <a class="navbar-brand hidden-xs hidden-sm" href="#home"><h1>Donated</h1></a>
+	          <a class="navbar-brand hidden-xs hidden-sm" href="#home"><h1 style="color:white;">Donated</h1></a>
 	        </div>
 	        <div class="navbar-collapse collapse">
 	          	<ul class="nav navbar-nav">
-		            <li><a href="<?= BASEURL; ?>/#home" class="smoothScroll">Home</a></li>
-					<li> <a href="<?= BASEURL; ?>/#about" class="smoothScroll">Tentang Kami</a></li>
-					<li> <a href="<?= BASEURL; ?>/#team" class="smoothScroll">Testimoni</a></li>
-					<li> <a href="<?= BASEURL; ?>/penerima" class="smoothScroll">Pengumuman</a></li>
-					<li> <a href="<?= BASEURL; ?>/#portfolio" class="smoothScroll">Kegiatan</a></li>
-					<li> <a href="<?= BASEURL; ?>/#daftarbeasiswa" class="smoothScroll">Apply</a></li>
+		            <li><a href="<?= BASEURL; ?>/#home" class="smoothScroll" style="color:white;">Home</a></li>
+					<li> <a href="<?= BASEURL; ?>/#about" class="smoothScroll" style="color:white;">Tentang Kami</a></li>
+					<li> <a href="<?= BASEURL; ?>/#team" class="smoothScroll" style="color:white;">Testimoni</a></li>
+					<li> <a href="<?= BASEURL; ?>/penerima" class="smoothScroll" style="color:white;">Pengumuman</a></li>
+					<li> <a href="<?= BASEURL; ?>/#portfolio" class="smoothScroll" style="color:white;">Kegiatan</a></li>
+					<li> <a href="<?= BASEURL; ?>/#daftarbeasiswa" class="smoothScroll" style="color:white;">Apply</a></li>
 				</ul>
 	        </div><!--/.nav-collapse -->
 	      </div>
